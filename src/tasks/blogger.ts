399ca6d23777746d1c2743c9041947b14e0cbc42
Michal Lata <michallata.dev@gmail.com>
2023-10-31 17:56:28 +0100

    Update blogger task

--- src/tasks/blogger.ts
+++ src/tasks/blogger.ts
@@ -1,5 +1,7 @@
-import { TasksProvider } from "../services";
-import { createBlogPost } from "../utils";
+import { BLOG_POST_CREATOR_CONTEXT } from "../constants";
+
+import { CompletionMessage } from "../services/types";
+import { OpenaiProvider, TasksProvider } from "../services";
 
 (async () => {
   const { blog } = await TasksProvider.getTask("blogger");
@@ -10,3 +12,28 @@ import { createBlogPost } from "../utils";
   const answerResponse = await TasksProvider.sendAnswer(answers);
   console.log(answerResponse);
 })();
+
+async function createBlogPost(inputData: string[]) {
+  const generatedChapters: string[] = [];
+
+  const inputMessages: CompletionMessage[] = inputData.map((input) => ({
+    role: "user",
+    content: input,
+  }));
+
+  for (const key in inputMessages) {
+    const messages: CompletionMessage[] = [];
+
+    messages.push({ role: "system", content: BLOG_POST_CREATOR_CONTEXT });
+    messages.push(inputMessages[key]);
+
+    const completionResponse = await OpenaiProvider.getCompletion(messages);
+    if (!completionResponse) throw new Error("completion error");
+
+    const { message } = completionResponse.choices[0];
+
+    generatedChapters.push(message.content!);
+  }
+
+  return generatedChapters;
+}
