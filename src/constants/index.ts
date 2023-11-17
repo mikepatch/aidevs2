7b830c1d3efe13970ef7f0a858e3618fa56bf573
Michal Lata <michallata.dev@gmail.com>
2023-11-17 14:24:07 +0100

    Add MODEL_NAMES constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -16,12 +16,15 @@ export const TASK_NAMES = {
   tools: "tools",
 };
 
-export const BLOG_POST_CREATOR_CONTEXT = `As a famous food blogger you create a wonderful and excellent content based on provided paragraph title
-###
-Rules:
-1. Write only about the food.
-2. Be creative.
-3. Write 3-5 sentences about provided paragraph title.
-4. Answer only in polish language.
-###
-`;
+export const MODEL_NAMES = {
+  gpt_4: "gpt-4-0613",
+  gpt_4_turbo: "gpt-4-1106-preview",
+  gpt_3_5_turbo: "gpt-3.5-turbo",
+  gpt_3_5_turbo_1106: "gpt-3.5-turbo-1106",
+  embeddings: "text-embedding-ada-002",
+  whisper: "whisper-1",
+  text_to_speech: "tts-1",
+  text_to_speech_HD: "tts-1-hd",
+  dall_e_3: "dall-e-3",
+  dall_e_2: "dall-e-2",
+};
