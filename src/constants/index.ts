f2f66416db8a99536e9b6de96c9f94edc8d0cf56
Michal Lata <michallata.dev@gmail.com>
2023-11-17 18:58:28 +0100

    Add gpt 4 vision to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -20,6 +20,7 @@ export const TASK_NAMES = {
 export const MODEL_NAMES = {
   gpt_4: "gpt-4-0613",
   gpt_4_turbo: "gpt-4-1106-preview",
+  gpt_4_vision: "gpt-4-vision-preview",
   gpt_3_5_turbo: "gpt-3.5-turbo",
   gpt_3_5_turbo_1106: "gpt-3.5-turbo-1106",
   embeddings: "text-embedding-ada-002",
