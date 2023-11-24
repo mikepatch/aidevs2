7418076323dc7564a38bb1c6c842bbafa0e8fe43
Michal Lata <michallata.dev@gmail.com>
2023-11-24 21:41:26 +0100

    Add google task solution

--- src/tasks/google/google.ts
+++ src/tasks/google/google.ts
@@ -1,3 +1,12 @@
-(async () => {
+import {TasksProvider} from "../../services";
+import {TASK_NAMES} from "../../constants";
 
+(async () => {
+    const API_URL = "https://miketest.bieda.it/search";
+    const taskInit = await TasksProvider.getTask(TASK_NAMES.google);
+    if(!taskInit) throw new Error('Problem with task init');
+
+    const answerResponse = await TasksProvider.sendAnswer(API_URL);
+
+    console.log(answerResponse);
 })()
